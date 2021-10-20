e60b6c79e6a6645aecb42fda88f9d67b7a94cf9f
Алексей Решетняк <lexsparrow@yandex.ru>
2021-10-20 10:22:09 +0300

    Add @ConditionalOnProperty

--- src/main/java/ru/netology/conditional/ConditionalApplication.java
+++ src/main/java/ru/netology/conditional/ConditionalApplication.java
@@ -2,22 +2,9 @@ package ru.netology.conditional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class ConditionalApplication {
-
-    @Bean
-    @Primary
-    public SystemProfile devProfile() {
-        return new DevProfile();
-    }
-
-    @Bean
-    public SystemProfile prodProfile() {
-        return new ProductionProfile();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConditionalApplication.class, args);
